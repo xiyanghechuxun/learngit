1df820e4bbc4e52b756b0fa5f062b5d690e560e0
huangyu <457969743@qq.com>
2018-08-23 22:43:44 +0800

    four test

--- function.js
+++ function.js
@@ -5,3 +5,13 @@ eq:function(1){
 	this.slice(i);
 	this.slice(i,i+1);
 }
+
+
+each:function(callback,args){
+	return $.each(this.callback,args);
+},
+
+ready:function(fn){
+	//add the callback
+	$.ready.promist().done(fn);
+}
